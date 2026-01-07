34eb79c95b47ea77a7333506cfe9c0b8320eea50
Maksym Murenets <skrimersik@gmail.com>
2026-01-08 01:17:20 +0200

    (Common) Feat: Updating offset on drag

--- common/src/space.rs
+++ common/src/space.rs
@@ -17,12 +17,30 @@ pub struct SpaceContext {
 #[derive(Debug)]
 pub struct SpaceSettings {
     pub zero_point: Point2D,
-    pub center: Point2D,
     pub unit_length: f64,
+    pub is_drag_enabled: bool,
 }
 
 #[derive(Debug)]
 pub struct SpaceState {
     pub pixels_per_centimeter: f64,
     pub offset: (f64, f64),
+}
+
+impl SpaceState {
+    pub fn update_offset(
+        &mut self, ui: &egui::Ui, response: &egui::Response, settings: &SpaceSettings,
+    ) {
+        if settings.is_drag_enabled && response.dragged() {
+            ui.ctx().set_cursor_icon(egui::CursorIcon::Grab);
+
+            let delta = response.drag_delta();
+            let dragging_coefficient = 1.0;
+
+            self.offset.0 += delta.x as f64 * dragging_coefficient;
+            self.offset.1 += delta.y as f64 * dragging_coefficient;
+
+            ui.ctx().request_repaint();
+        }
+    }
 }
