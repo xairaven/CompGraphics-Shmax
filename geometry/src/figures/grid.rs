53c8e6ce5d176b11108f76a767af74f8ae0af269
Maksym Murenets <skrimersik@gmail.com>
2026-01-10 20:29:54 +0200

    (Geometry) Feat: Introduce GridBounds struct for improved axis bounds handling

--- geometry/src/figures/grid.rs
+++ geometry/src/figures/grid.rs
@@ -1,6 +1,6 @@
 use crate::primitives::line2d::Line2D;
 use crate::primitives::point2d::Point2D;
-use crate::units::Centimeter;
+use crate::units::{Centimeter, Pixel};
 use crate::viewport::Viewport;
 use egui::Stroke;
 
@@ -19,7 +19,7 @@ pub mod defaults {
     };
 
     pub const AXIS_RED: Stroke = Stroke {
-        width: 50.0,
+        width: 2.0,
         color: Color32::RED,
     };
     pub const AXIS_GREEN: Stroke = Stroke {
@@ -98,11 +98,41 @@ impl Grid2DBuilder {
             is_enabled: true,
             origin: self.origin,
             units: self.units,
-            bounds_x: self.bounds_x,
-            bounds_y: self.bounds_y,
+            bounds: GridBounds {
+                x: (self.bounds_x.0, self.bounds_x.1),
+                y: (self.bounds_y.0, self.bounds_y.1),
+            },
             axes_strokes: self.axes_strokes,
             grid_stroke: self.grid_stroke,
         }
+    }
+}
+
+pub struct GridBounds {
+    pub x: (Option<Centimeter>, Option<Centimeter>),
+    pub y: (Option<Centimeter>, Option<Centimeter>),
+}
+
+impl GridBounds {
+    fn find_minimum(
+        screen_bound: Pixel, user_bound: Option<Centimeter>, viewport: &Viewport,
+    ) -> Centimeter {
+        let screen_bound = screen_bound.to_centimeters(viewport);
+        user_bound.map_or(screen_bound, |b| Centimeter(b.min(screen_bound.value())))
+    }
+
+    pub fn find_minimums(&self, viewport: &Viewport) -> (Centimeter, Centimeter) {
+        (
+            Self::find_minimum(viewport.state.bounds.minimum_x, self.x.0, viewport),
+            Self::find_minimum(viewport.state.bounds.minimum_y, self.x.0, viewport),
+        )
+    }
+
+    pub fn find_maximums(&self, viewport: &Viewport) -> (Centimeter, Centimeter) {
+        (
+            Self::find_minimum(viewport.state.bounds.maximum_x, self.x.1, viewport),
+            Self::find_minimum(viewport.state.bounds.maximum_y, self.x.1, viewport),
+        )
     }
 }
 
@@ -112,8 +142,7 @@ pub struct Grid2D {
     pub origin: Point2D,
     pub units: Point2D,
 
-    pub bounds_x: (Option<Centimeter>, Option<Centimeter>),
-    pub bounds_y: (Option<Centimeter>, Option<Centimeter>),
+    pub bounds: GridBounds,
 
     pub axes_strokes: (Stroke, Stroke),
     pub grid_stroke: Stroke,
@@ -122,6 +151,35 @@ pub struct Grid2D {
 impl Grid2D {
     pub fn lines(&self, viewport: &Viewport) -> Vec<Line2D<Point2D>> {
         let mut lines = vec![];
+
+        let (minimum_x, minimum_y) = self.bounds.find_minimums(viewport);
+        let (maximum_x, maximum_y) = self.bounds.find_maximums(viewport);
+
+        let axis_x = Line2D {
+            start: Point2D {
+                x: minimum_x,
+                y: self.origin.y,
+            },
+            end: Point2D {
+                x: maximum_x,
+                y: self.origin.y,
+            },
+            stroke: self.axes_strokes.0,
+        };
+        lines.push(axis_x);
+
+        let axis_y = Line2D {
+            start: Point2D {
+                x: self.origin.x,
+                y: minimum_y,
+            },
+            end: Point2D {
+                x: self.origin.x,
+                y: maximum_y,
+            },
+            stroke: self.axes_strokes.1,
+        };
+        lines.push(axis_y);
 
         lines
     }
