beacf795eb9055a841313059ed33be1c143b0cd7
Maksym Murenets <skrimersik@gmail.com>
2026-01-10 18:48:27 +0200

    (Geometry) Refactor: Simplify pixel and centimeter conversion calculations in viewport

--- geometry/src/conversion.rs
+++ geometry/src/conversion.rs
@@ -4,32 +4,24 @@ use crate::viewport::Viewport;
 
 impl Centimeter {
     pub fn to_pixels(self, viewport: &Viewport) -> Pixel {
-        Pixel(
-            self.value() / viewport.geometry.unit_length.value()
-                * viewport.geometry.pixels_per_centimeter,
-        )
+        Pixel(self.value() * viewport.geometry.pixels_per_centimeter)
     }
 }
 
 impl Pixel {
     pub fn to_centimeters(self, viewport: &Viewport) -> Centimeter {
-        Centimeter(
-            self.value() / viewport.geometry.pixels_per_centimeter
-                * viewport.geometry.unit_length.value(),
-        )
+        Centimeter(self.value() / viewport.geometry.pixels_per_centimeter)
     }
 }
 
 impl Point2D {
     pub fn to_pixels(self, viewport: &Viewport) -> Point2DPixel {
         let x = viewport.state.zero_point.x.value()
-            + (self.x.value() / viewport.geometry.unit_length.value()
-                * viewport.geometry.pixels_per_centimeter)
+            + (self.x.value() * viewport.geometry.pixels_per_centimeter)
             + viewport.geometry.offset.x.value();
 
         let y = viewport.state.zero_point.y.value()
-            - (self.y.value() / viewport.geometry.unit_length.value()
-                * viewport.geometry.pixels_per_centimeter)
+            - (self.y.value() * viewport.geometry.pixels_per_centimeter)
             + viewport.geometry.offset.y.value();
 
         Point2DPixel::new(x, y)
@@ -38,15 +30,15 @@ impl Point2D {
 
 impl Point2DPixel {
     pub fn to_centimeters(self, viewport: &Viewport) -> Point2D {
-        let x = (self.x.value() * viewport.geometry.unit_length.value())
-            / viewport.geometry.pixels_per_centimeter
+        let x = (self.x.value()
             - viewport.state.zero_point.x.value()
-            + viewport.geometry.offset.x.value();
+            - viewport.geometry.offset.x.value())
+            / viewport.geometry.pixels_per_centimeter;
 
-        let y = (self.y.value() * viewport.geometry.unit_length.value())
-            / viewport.geometry.pixels_per_centimeter
+        let y = (-self.y.value()
             + viewport.state.zero_point.y.value()
-            + viewport.geometry.offset.y.value();
+            + viewport.geometry.offset.y.value())
+            / viewport.geometry.pixels_per_centimeter;
 
         Point2D::new(x, y)
     }
