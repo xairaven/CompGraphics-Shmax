73c2307c54b466bc167fa72479ce227cc23bafd5
Maksym Murenets <skrimersik@gmail.com>
2026-01-15 19:13:59 +0200

    (Lab 3) Init

---
+++ Lab3/src/context.rs
@@ -0,0 +1,82 @@
+use crate::ui::modals::error::ErrorModal;
+use crate::utils::channel::Channel;
+use geometry::animations::epicycloid::AnimationEpicycloid;
+use geometry::animations::walker::CurveWalker;
+use geometry::figures::epicycloid::Epicycloid;
+use geometry::figures::grid::{Grid2D, Grid2DBuilder};
+use geometry::pipeline::Pipeline;
+use geometry::transformations::euclidean::offset::EuclideanOffset;
+use geometry::transformations::euclidean::rotation::EuclideanRotation;
+use geometry::units::Centimeter;
+use geometry::viewport::{Viewport, ViewportGeometry, ViewportState, ZeroPointLocation};
+
+#[derive(Debug)]
+pub struct Context {
+    pub figures: FiguresState,
+    pub transformations: TransformContext,
+    pub animations: AnimationsContext,
+    pub viewport: Viewport,
+    pub errors_channel: Channel<ErrorModal>,
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        Self {
+            figures: FiguresState::default(),
+            transformations: TransformContext::default(),
+            animations: AnimationsContext::default(),
+
+            viewport: Viewport {
+                // Default settings like panning, zooming, etc.
+                config: Default::default(),
+                // Default geometry settings, can be updated by user
+                geometry: ViewportGeometry {
+                    zero_point_location: ZeroPointLocation::Center,
+                    ..Default::default()
+                },
+                // Initial viewport state, will be updated when the UI is built
+                state: ViewportState::default(),
+            },
+
+            errors_channel: Channel::default(),
+        }
+    }
+}
+
+impl Context {
+    pub fn reset(&mut self) {
+        *self = Default::default();
+    }
+}
+
+#[derive(Debug)]
+pub struct FiguresState {
+    pub grid: Grid2D,
+    pub grid_pipeline: Pipeline,
+
+    pub epicycloid: Epicycloid,
+    pub epicycloid_pipeline: Pipeline,
+}
+
+impl Default for FiguresState {
+    fn default() -> Self {
+        Self {
+            grid: Grid2DBuilder::default().with_unit(Centimeter(5.0)).build(),
+            grid_pipeline: Default::default(),
+            epicycloid: Default::default(),
+            epicycloid_pipeline: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct TransformContext {
+    pub offset: EuclideanOffset,
+    pub rotation: EuclideanRotation,
+}
+
+#[derive(Debug, Default)]
+pub struct AnimationsContext {
+    pub epicycloid: AnimationEpicycloid,
+    pub walker: CurveWalker,
+}
