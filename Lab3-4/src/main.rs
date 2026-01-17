a4a79fa45a512643762cde0be3820b3168fcb3b7
Maksym Murenets <skrimersik@gmail.com>
2026-01-17 03:13:24 +0200

    (Lab 3-4) Fix: Update project title

--- Lab3-4/src/main.rs
+++ Lab3-4/src/main.rs
@@ -5,7 +5,7 @@ use crate::config::Config;
 use crate::logs::Logger;
 use crate::ui::Ui;
 
-const PROJECT_TITLE: &str = "Лабораторна робота №3";
+const PROJECT_TITLE: &str = "Лабораторна робота №3-4";
 
 fn main() {
     let config = Config::from_file().unwrap_or_else(|error| {
